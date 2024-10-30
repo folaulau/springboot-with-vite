5b85b6b8ff17b6f635feb7b10731c7b7230e8f27
Folau Kaveinga <folaukaveinga@gmail.com>
2024-10-29 18:12:10 -0600

    asdf

---
+++ src/main/java/com/folau/springboot/SpringbootWithViteApplication.java
@@ -0,0 +1,13 @@
+package com.folau.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithViteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithViteApplication.class, args);
+	}
+
+}
